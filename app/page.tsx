a22605116d7f817dae65298c2fc00fc655028658
nikhilr234-stack <nikhilr234@gmail.com>
2025-12-28 12:47:54 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Vastu Planning Tool</h1>
+      <p>If you can see this, the website is working.</p>
+    </main>
+  );
+}
